f7c308ab6c522876355b0fca721db5d31a9f25d1
Carsten König <Carsten@gettingsharper.de>
2016-12-02 11:20:16 +0100

    Day2: Start 2

--- Day2/Main.hs
+++ Day2/Main.hs
@@ -32,35 +32,54 @@ data Row = Row
   , current :: Char
   , right :: [Char]
   }
+  deriving Show
 
 data KeyPad = KeyPad
   { over :: [Row]
   , row :: Row
   , under :: [Row]
   }
+  deriving Show
 
 getNumber :: KeyPad -> Char
 getNumber = current . row
 
 moveRow :: Move -> Row -> Row
-moveRow MoveLeft (Row (l:ls) c rs) = Row ls l (c:rs)
-moveRow MoveRight (Row ls c (r:rs)) = Row (c:ls) r rs
+moveRow MoveLeft (Row (l:ls) c rs) =
+  Row ls l (c:rs)
+moveRow MoveRight (Row ls c (r:rs)) =
+  Row (c:ls) r rs
 moveRow _ r = r
 
 move :: KeyPad -> Move -> KeyPad
-move (KeyPad (o:os) r us) MoveUp = KeyPad os o (r:us)
-move (KeyPad os r (u:us)) MoveDown = KeyPad (r:os) u us
-move (KeyPad os r us) mv =
-  KeyPad (fmap (moveRow mv) os) (moveRow mv r) (fmap (moveRow mv) us)
+move (KeyPad (o:os) r us) MoveUp
+  | current o /= '0' = KeyPad os o (r:us)
+move (KeyPad os r (u:us)) MoveDown
+  | current u /= '0' = KeyPad (r:os) u us
+move kp@(KeyPad os r us) mv =
+  let r' = moveRow mv r
+  in if current r' /= '0'
+     then KeyPad (fmap (moveRow mv) os) r' (fmap (moveRow mv) us)
+     else kp
 
-start :: KeyPad
-start = KeyPad [] (Row [] '1' "23") [Row [] '4' "56", Row [] '7' "89"]
+normalPad :: KeyPad
+normalPad = KeyPad [] (Row [] '1' "23") [Row [] '4' "56", Row [] '7' "89"]
 
-start5 :: KeyPad
-start5 = move (move start MoveRight) MoveDown
+fancyPad :: KeyPad
+fancyPad = KeyPad
+  []
+  (Row "00" '1' "00")
+  [ Row "20" '3' "40"
+  , Row "65" '7' "89"
+  , Row "A0" 'B' "C0"
+  , Row "00" 'D' "00"
+  ]
 
 moves :: KeyPad -> [Move] -> KeyPad
 moves s = foldl' move s
+
+start5 :: KeyPad
+start5 = moves fancyPad [MoveDown, MoveDown, MoveLeft, MoveLeft]
 
 addNumber :: (KeyPad, String) -> [Move] -> (KeyPad, String)
 addNumber (kp, acc) mvs =
