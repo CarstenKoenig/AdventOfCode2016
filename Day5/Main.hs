2805f2b3e3d666685eb999a87f23d26daae0c6d5
Carsten König <Carsten@gettingsharper.de>
2016-12-05 08:23:15 +0100

    started day 5

---
+++ Day5/Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Data.List (group, sort, find, isPrefixOf)
+import Data.Maybe (catMaybes, isJust)
+import Data.Char (toLower, isLetter, ord, chr)
+
+main :: IO ()
+main = do
+  print "solve me"
+
+
+input :: String
+input = "reyedfim"
