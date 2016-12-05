73956f2e81f0aad316295cdec282807a218f947f
Carsten König <Carsten@gettingsharper.de>
2016-12-05 08:14:39 +0100

    day 4: 2 stars

--- Day4/Main.hs
+++ Day4/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import Data.List (group, sort)
-import Data.Maybe (catMaybes)
-import Data.Char (toLower, isDigit, isSpace, isAlpha)
+import Data.List (group, sort, find, isPrefixOf)
+import Data.Maybe (catMaybes, isJust)
+import Data.Char (toLower, isLetter, ord, chr)
 
 import Parser
 
@@ -39,6 +39,33 @@ occurences = sort . groups . sort . concat . tokenParts
 isReal :: SecToken -> Bool
 isReal token = tokenCheck token == check
   where check = take 5 . map snd $ occurences token
+
+
+shiftChar :: Char -> Char
+shiftChar '-' = ' '
+shiftChar 'z' = 'a'
+shiftChar 'Z' = 'A'
+shiftChar c
+  | isLetter c = succ c
+  | otherwise = c
+
+
+nTimes :: (a -> a) -> Int -> a -> a
+nTimes _ 0 x = x
+nTimes f n x = nTimes f (n-1) (f x)
+
+
+decrypt :: SecToken -> [String]
+decrypt token =
+  map (map decode) $ tokenParts token
+  where decode = nTimes shiftChar n
+        n = tokenId token `mod` 26
+
+
+containsNorth :: SecToken -> Bool
+containsNorth token =
+  any (isPrefixOf "north") wrds
+  where wrds = decrypt token
   
 
 main :: IO ()
@@ -46,9 +73,11 @@ main = do
   contents <- lines <$> readFile "input.txt"
   let inputs = catMaybes $ map (eval parseSecToken) contents
   let reals = filter isReal inputs
+  let withN = filter containsNorth reals
+  print withN
   let output = sum $ map tokenId reals
   print output
 
 
 testInput :: String
-testInput = "aaaaa-bbb-z-y-x-123[abxyz]"
+testInput = "qzmt-zixmtkozy-ivhz-343[abcde]"
