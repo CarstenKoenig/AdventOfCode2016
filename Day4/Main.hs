54ee0351f00b3f7e45ce6b51c7456ee152f54e23
Carsten König <Carsten@gettingsharper.de>
2016-12-05 07:13:51 +0100

    day4 : one star

--- Day4/Main.hs
+++ Day4/Main.hs
@@ -1,103 +1,54 @@
 module Main where
 
-import Data.List (foldl', concatMap, transpose)
+import Data.List (group, sort)
 import Data.Maybe (catMaybes)
-import Data.Char (toLower, isDigit, isSpace)
+import Data.Char (toLower, isDigit, isSpace, isAlpha)
 
+import Parser
 
-
-data Triangle
-  = Triangle Int Int Int
+data SecToken
+  = SecToken
+    { tokenParts :: [String]
+    , tokenId :: Int
+    , tokenCheck :: String
+    }
   deriving Show
 
 
-parseDigits :: String -> Maybe (String, String)
-parseDigits "" = Just ("", "")
-parseDigits (c : remainder)
-  | isDigit c = do
-      (ds, rem) <- parseDigits remainder
-      return (c:ds, rem)
-  | otherwise = Just ("", c:remainder)
+parseSecToken :: Parser SecToken
+parseSecToken = do
+  parts <- parseParts
+  id <- parseNumber
+  _ <- parseChar (== '[')
+  check <- parseAlphas
+  _ <- parseChar (== ']')
+  return $ SecToken parts id check
+  where parseParts = parseMany parsePart
+        parsePart = do
+          str <- parseAlphas
+          _ <- parseChar (== '-')
+          return str
+          
+
+occurences :: SecToken -> [(Int, Char)]
+occurences = sort . groups . sort . concat . tokenParts
+  where groups = map getGroup . group
+        getGroup xs@(x:_) = (negate (length xs), x)
 
 
-parseNumber :: String -> Maybe (Int, String)
-parseNumber input = do
-  ((), rem) <- parseWhiteSpace input
-  (ds, rem') <- parseDigits rem
-  ((), rem'') <- parseWhiteSpace rem'
-  case ds of
-    "" -> Nothing
-    ds -> Just (read ds, rem'')
-
-
-parseWhiteSpace :: String -> Maybe ((), String)
-parseWhiteSpace "" = Just ((), "")
-parseWhiteSpace input@(c : remainder)
-  | isSpace c = parseWhiteSpace remainder
-  | otherwise = Just ((), input)
-
-
-parseTriangle :: String -> Maybe (Triangle, String)
-parseTriangle input =
-  case parseNumber input of
-    Nothing -> Nothing
-    Just (nr1, rem) ->
-      case parseNumber rem of
-        Nothing -> Nothing
-        Just (nr2, rem2) ->
-          case parseNumber rem2 of
-            Nothing -> Nothing
-            Just (nr3, rem3) ->
-              Just (Triangle nr1 nr2 nr3, rem3)
-
-
-readThreeNumbers :: String -> Maybe [Int]
-readThreeNumbers input =
-  case parseNumber input of
-    Nothing -> Nothing
-    Just (nr1, rem) ->
-      case parseNumber rem of
-        Nothing -> Nothing
-        Just (nr2, rem2) ->
-          case parseNumber rem2 of
-            Nothing -> Nothing
-            Just (nr3, rem3) ->
-              Just [nr1, nr2, nr3]
-
-
-justNumbers :: [String] -> [[Int]]
-justNumbers = catMaybes . map readThreeNumbers
-
-
-readBlocks :: [[Int]] -> [[[Int]]]
-readBlocks (a:b:c:rest) = [a,b,c] : readBlocks rest
-readBlocks rest = [rest]
-
-
-toTriangles :: [[Int]] -> [Triangle]
-toTriangles = map toTriangle
-  where toTriangle [a,b,c] = Triangle a b c
+isReal :: SecToken -> Bool
+isReal token = tokenCheck token == check
+  where check = take 5 . map snd $ occurences token
   
-
-getLines :: IO [String]
-getLines = do
-  content <- getContents
-  return $ lines content
-
-
-probableTriangle :: Triangle -> Bool
-probableTriangle (Triangle a b c) =
-  a+b > c && a+c > b && b+c > a
-
 
 main :: IO ()
 main = do
-  lines <- getLines
-  let blocks = readBlocks . justNumbers $ lines
-  let triangles = concatMap toTriangles $ map transpose blocks
-  let count = length $ filter probableTriangle triangles
-  print count
+  contents <- lines <$> readFile "input.txt"
+  let inputs = catMaybes $ map (eval parseSecToken) contents
+  let reals = filter isReal inputs
+  let output = sum $ map tokenId reals
+  print output
 
 
 testInput :: String
-testInput = "    4   21  894"
+testInput = "aaaaa-bbb-z-y-x-123[abxyz]"
