fa5e4a24c67751f7bdd9d09fb0c2bb218558a415
Carsten König <carsten@gettingsharper.de>
2016-12-10 10:37:26 +0100

    day 10: parse input

--- Day10/Main.hs
+++ Day10/Main.hs
@@ -1,13 +1,90 @@
 module Main where
 
 import Parser
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, catMaybes)
 
 
-input :: IO String
-input = readFile "input.txt"
+type BotNr = Int
+
+newtype Bot
+  = Bot BotNr
+  deriving (Eq, Show)
+
+
+newtype Value
+  = Value Int
+  deriving (Eq, Show)
+
+data Target
+  = ToBot Bot
+  | Output Int
+  deriving (Eq, Show)
+
+
+data Instruction
+  = BotRule Bot Target Target
+  | Input Bot Value
+  deriving (Eq, Show)
+
+
+parseInstruction :: Parser Instruction
+parseInstruction =
+  parseEither parseInput parseBotRule
+
+  
+parseInput :: Parser Instruction
+parseInput = do
+  val <- parseValue
+  parseString "goes to "
+  bot <- parseBot
+  return $ Input bot val
+
+
+parseBotRule :: Parser Instruction
+parseBotRule = do
+  bot <- parseBot
+  parseString "gives low "
+  low <- parseTarget
+  parseString "and high "
+  high <- parseTarget
+  return $ BotRule bot low high
+  
+
+parseTarget :: Parser Target
+parseTarget = do
+  parseString "to "
+  parseEither parseOutput (ToBot <$> parseBot)
+  where
+    parseOutput = do
+      parseString "output "
+      Output <$> parseNumber
+
+
+parseBot :: Parser Bot      
+parseBot = do
+      parseString "bot "
+      Bot <$> parseNumber
+
+
+parseValue :: Parser Value
+parseValue = do
+      parseString "value "
+      Value <$> parseNumber
+
+
+input :: IO [Instruction]
+input = catMaybes . map (eval parseInstruction) . lines <$> readFile "input.txt"
+
 
 main :: IO ()
 main = do
-  text <- input
+  instr <- input
+  print instr
   putStrLn "all done"
+
+
+test :: String
+test = "value 5 goes to bot 2"
+
+test' :: String
+test' = "bot 0 gives low to output 2 and high to output 0"
