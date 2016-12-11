9a1353a7b87299177927ce429fb376f829dd1fb0
Carsten König <carsten@gettingsharper.de>
2016-12-11 19:20:27 +0100

    day 11: ugly hacking - slow running non-solution :(

--- Day11/Main.hs
+++ Day11/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Data.List (delete)
+import Data.List (delete, lookup, sortBy)
+import Data.Function (on)
 import Data.Maybe (catMaybes, mapMaybe)
 import Data.Set (Set)
 import qualified Data.Set as S
@@ -8,14 +9,13 @@ import qualified Data.Set as S
 import Model
 import Parser (eval)
 
-
 data State =
   State { elevatorAt :: Int
         , floor1 :: Set Item
         , floor2 :: Set Item
         , floor3 :: Set Item
         , floor4 :: Set Item
-        } deriving (Eq, Show)
+        } deriving (Eq, Show, Ord)
 
 
 data Direction
@@ -23,6 +23,115 @@ data Direction
   deriving (Eq, Show, Ord, Enum)
 
 
+data EqState =
+  EqState { eqElevator :: Int
+          , eqFloor1 :: [EqItem]
+          , eqFloor2 :: [EqItem]
+          , eqFloor3 :: [EqItem]
+          , eqFloor4 :: [EqItem]
+          } deriving (Eq, Show, Ord)
+
+data EqItem
+  = Chip Int
+  | Gen Int
+  deriving (Eq, Show, Ord)
+
+
+stateToEq :: State -> EqState
+stateToEq state =
+  let (eqF1, assocs1) = itemsToEq [] (S.toList $ floor1 state)
+      (eqF2, assocs2) = itemsToEq assocs1 (S.toList $ floor2 state)
+      (eqF3, assocs3) = itemsToEq assocs2 (S.toList $ floor3 state)
+      (eqF4, _) = itemsToEq assocs3 (S.toList $ floor4 state)
+  in EqState (elevatorAt state) eqF1 eqF2 eqF3 eqF4
+  
+
+itemsToEq :: [(Element, Int)] -> [Item] -> ([EqItem], [(Element, Int)])
+itemsToEq assocs [] = ([], assocs)
+itemsToEq assocs (i:is) =
+  let (i', assocs') = itemToEq assocs i
+      (is',assocs'') = itemsToEq assocs' is
+  in (i':is',assocs'')
+
+
+itemToEq :: [(Element, Int)] -> Item -> (EqItem, [(Element, Int)])
+itemToEq assocs item =
+  let el = element item
+      next = length assocs
+  in 
+    case lookup el assocs of
+      Nothing -> (toEq item next, (el,next):assocs)
+      Just ind -> (toEq item ind, assocs)
+  where toEq (Microchip _) i = Chip i
+        toEq (Generator _) i = Gen i
+
+
+element :: Item -> Element
+element (Microchip el) = el
+element (Generator el) = el
+
+
+solutions :: State -> [(Integer, State)]
+solutions state =
+  findSolutions S.empty [(0,state)]
+
+
+findSolutions :: Set EqState -> [(Integer, State)] -> [(Integer, State)]
+findSolutions _ [] = []
+findSolutions visited ((steps, state):nexts) =
+  let visited' = S.insert (stateToEq state) visited
+      nexts' = nexts ++ (map (\st -> (steps+1,st)) $ possibleFrom visited' state)
+      sols = findSolutions visited' (prevereUp nexts')
+  in if isSolution state
+     then (steps, state) : sols
+     else sols
+  where
+    prevereUp list = sortBy (compare `on` evaluate) list
+    evaluate (cnt, state) =
+      let pts =
+            length (floor3 state)
+            + 2 * length (floor2 state)
+            + 4 * length (floor1 state)
+      in (cnt, pts)
+
+
+
+possibleFrom :: Set EqState -> State -> [State]
+possibleFrom visited state =
+  [ s | d <- directions state
+      , (i1, i2o) <- pickItems d state
+      , let s = move d i1 i2o state
+      , validState s
+      , not (S.member (stateToEq s) visited)
+  ]
+
+
+isSolution :: State -> Bool
+isSolution state =
+  null (floor1 state)
+  && null (floor2 state)
+  && null (floor3 state)
+
+
+pickItems :: Direction -> State -> [ (Item, Maybe Item) ]
+pickItems Up state =
+  pickMaybeTwo (S.toList $ getFloorItems (elevatorAt state) state)
+pickItems Down state =
+  reverse $ pickMaybeTwo (S.toList $ getFloorItems (elevatorAt state) state)
+
+
+pickMaybeTwo :: [a] -> [ (a, Maybe a)]
+pickMaybeTwo items =
+  [ (x, Just y) | (x,items') <- picks items, (y, _) <- picks items' ]
+  ++ [ (x, Nothing) | (x,_) <- picks items ]
+
+
+picks :: [a] -> [(a,[a])]
+picks (x:xs) =
+  (x,xs) : [ (y,ys) | (y,ys) <- picks xs ]
+picks [] = []
+
+  
 move :: Direction -> Item -> Maybe Item -> State -> State
 move dir item1 item2opt state =
   let start = elevatorAt state
@@ -31,7 +140,7 @@ move dir item1 item2opt state =
         changeFloor (removeItem item2opt . removeItem (Just item1)) start state
       state'' =
         changeFloor (addItem item2opt . addItem (Just item1)) dest state'
-  in state''
+  in state'' { elevatorAt = dest }
 
 
 validState :: State -> Bool
@@ -122,7 +231,51 @@ puzzle = do
     catMaybes . fmap (eval parseFloor) <$> input
   return $ State 1 f1 f2 f3 f4
 
+
+solve :: State -> Maybe Integer
+solve state =
+  let sols = solutions state
+  in case sols of
+      [] -> Nothing
+      ((steps,_):_) -> Just steps
+
+
+solvePuzzle :: IO (Maybe Integer)
+solvePuzzle = solve <$> puzzle
+
+
 main :: IO ()
 main = do
-  floorInputs <- input
+  steps <- solvePuzzle
+  putStrLn $ "It took " ++ show steps ++ " steps"
   putStrLn "all done"
+
+
+testPuzzle :: State
+testPuzzle =
+  State
+    1
+    (S.fromList [Microchip "H", Microchip "L"])
+    (S.fromList [Generator "H"])
+    (S.singleton (Generator "L"))
+    S.empty
+
+
+testPuzzle' :: State
+testPuzzle' =
+  State
+    2
+    (S.fromList [Microchip "L"])
+    (S.fromList [Microchip "H"])
+    (S.fromList [Generator "H", Generator "L"])
+    S.empty
+
+
+testPuzzle'' :: State
+testPuzzle'' =
+  State
+    3
+    (S.fromList [])
+    (S.fromList [])
+    (S.fromList [Microchip "H", Microchip "L"])
+    (S.fromList [Generator "H", Generator "L"])
