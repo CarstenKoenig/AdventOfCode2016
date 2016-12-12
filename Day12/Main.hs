0dec33e04702ce8080d2c804aab37417b348005e
Carsten König <Carsten@gettingsharper.de>
2016-12-12 07:25:42 +0100

    day 12: output

--- Day12/Main.hs
+++ Day12/Main.hs
@@ -220,10 +220,16 @@ programm2 =
 inputCPU2 :: IO CPU
 inputCPU2 = initialize <$> programm2
 
+
 main :: IO ()
 main = do
-  ls <- inputLines
+  p1 <- inputCPU
+  p2 <- inputCPU2
+  putStrLn $ "part 1: " ++ show (eval p1)
+  putStrLn $ "part 2: " ++ show (eval p2)
   putStrLn "all done"
+  where
+    eval p = regA $ run p
 
 
 testProgramm :: Programm
