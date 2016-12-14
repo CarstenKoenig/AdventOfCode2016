9ac41c91d9d8b1d405725effb66cb7f7fd84f873
Carsten König <Carsten@gettingsharper.de>
2016-12-14 09:51:36 +0100

    fixed day14

--- Day14/Main.hs
+++ Day14/Main.hs
@@ -28,6 +28,28 @@ salt :: L.ByteString
 salt = "cuanljph"
 -- salt = "abc"
 
+-- a bit hacky but it works for this
+-- if you want to be safe you have to inlcude
+-- keys until the sorted first 64 don't change
+-- anymore ... not lazy to do that right now
+the64th :: Int
+the64th =
+  (\ (a,_,_) -> a) . head . drop 63 . sort $ take 66 keys
+
+
+keys :: [(Int,Int,Char)]
+keys = concatMap fst steps
+  where 
+    steps = scanl step ([], []) candidates
+
+
+----------------------------------------------------------------------
+-- hashing
+puzzleHash :: L.ByteString -> Int -> L.ByteString
+puzzleHash input index =
+  hashMore 2017 $ input `L.append` (pack $ show index)
+
+
 hexify :: L.ByteString -> L.ByteString
 hexify = toLazyByteString . lazyByteStringHex
 
@@ -39,36 +61,19 @@ hashMore 0 xs = xs
 hashMore n input =
   hashMore (n-1) (hash input)
 
-puzzleHash :: L.ByteString -> Int -> L.ByteString
-puzzleHash input index =
-  hashMore 2017 $ input `L.append` (pack $ show index)
+
+----------------------------------------------------------------------
+-- list/stream generators
+candidates :: [(Int, Candidate)]
+candidates = mapMaybe containsCandidate inputStream
 
 
 inputStream :: [(Int, L.ByteString)]
 inputStream = map (\i -> (i, puzzleHash salt i)) [0..]
 
 
-candidates :: [(Int, Candidate)]
-candidates = mapMaybe containsCandidate inputStream
-
-
-the64th :: [(Int,Int,Char)]
-the64th = drop 63 . sort . filter validate $ take 66 keys
-
-keys :: [(Int,Int,Char)]
-keys = concatMap fst steps
-
-steps = scanl step ([], []) candidates
-
-
-contains :: L.ByteString -> L.ByteString -> Bool
-contains b a = any (a `isPrefixOf`) (tails b)
-
-
-validate :: (Int,Int,Char) -> Bool
-validate (ind,ind',c) =
-  puzzleHash salt ind `contains` (C8.replicate 3 c)
-  && puzzleHash salt ind' `contains` (C8.replicate 5 c)
+----------------------------------------------------------------------
+-- scaning for solutions
 
 containsCandidate :: (Int, L.ByteString) -> Maybe (Int, Candidate)
 containsCandidate inp =
@@ -99,6 +104,9 @@ containsTripple (i, input) =
    (i,) . Tripple . C8.head <$> (find ((>= 3) . C8.length) $ group input)
 
 
+----------------------------------------------------------------------
+-- folding using a lookup-table
+
 type Cache = [(Char, Int)]
 
 step :: ([(Int,Int,Char)], Cache) -> (Int, Candidate) -> ([(Int,Int,Char)], Cache)
@@ -106,8 +114,12 @@ step (_, cache) (ind, Tripple c) =
   ([], clear (ind-1000) $ insertIndex c ind cache)
 step (_, cache) (ind, Fives (cs@(c:_))) =
   let cache' = clear (ind-1000) cache
-      founds = (\ (ind',c) -> (ind',ind,c)) <$> concatMap (findIndizes cache') cs
-      cache'' = filter (\ (_,ind) -> not (ind `elem` map (\ (i,_,_) -> i) founds)) cache'
+      founds =
+        (\ (ind',c) -> (ind',ind,c))
+        <$> concatMap (findIndizes cache') cs
+      cache'' =
+        filter (\ (_,ind) -> not (ind `elem` map (\ (i,_,_) -> i) founds))
+        cache'
   in (founds, insertIndex c ind cache'')
 
 
