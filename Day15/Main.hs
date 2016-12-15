ea8972c7ff999651a1c28184516750bade72dd51
Carsten König <Carsten@gettingsharper.de>
2016-12-15 06:28:56 +0100

    Day 15: part 1

--- Day15/Main.hs
+++ Day15/Main.hs
@@ -1,5 +1,55 @@
 module Main where
 
+import Data.List (intersect, foldr1)
+
+type Time = Integer
+
+type Disc = (Int, Time -> Bool)
+
+createDisc :: Int -> Int -> Int -> Disc
+createDisc posCnt nr startPos =
+  let delta = (nr + startPos)  `mod` posCnt
+  in (posCnt, \ time -> (fromIntegral delta + time) `mod` fromIntegral posCnt == 0)
+
+
+firstZero :: Disc -> Time
+firstZero (_, slot) = head $ [ n | n <- [0..], slot n ]
+
+zeros :: Disc -> [Time]
+zeros disc@(n, slot) =
+  let fZ = firstZero disc
+      step = fromIntegral n
+  in [ fZ + n | n <- [0,step..] ]
+
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge as'@(a:as) bs'@(b:bs)
+  | a < b = merge as bs'
+  | b < a = merge as' bs
+  | a == b = a : merge as bs
+merge _ _ = []
+
+
+solutions :: [Disc] -> [Time]
+solutions = foldr1 merge . map zeros
+
+
+solution :: [Disc] -> Time
+solution = head . solutions
+
+
+test :: [Disc]
+test = [ createDisc 5 1 4, createDisc 2 2 1 ]
+
+part1 :: [Disc]
+part1 =
+  [ createDisc 13 1 11
+  , createDisc 5 2 0
+  , createDisc 17 3 11
+  , createDisc 3 4 0
+  , createDisc 7 5 2
+  , createDisc 19 6 17 ]
+
 main :: IO ()
 main = do
   putStrLn "all done"
