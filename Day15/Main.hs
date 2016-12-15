15bb5777a43e912b0ba93edc2f22b03e440ae7a6
Carsten König <Carsten@gettingsharper.de>
2016-12-15 07:01:40 +0100

    refactored Day15

--- Day15/Main.hs
+++ Day15/Main.hs
@@ -1,25 +1,74 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import Data.List (foldr1)
 
+
 type Time = Integer
-
-type Disc = (Int, Time -> Bool)
-
-createDisc :: Int -> Int -> Int -> Disc
-createDisc posCnt nr startPos =
-  let delta = (nr + startPos)  `mod` posCnt
-  in (posCnt, \ time -> (fromIntegral delta + time) `mod` fromIntegral posCnt == 0)
+data Disc =
+  Disc { possitions :: Int
+       , delta :: Int
+       }
+       
 
 
-firstZero :: Disc -> Time
-firstZero (_, slot) = head $ [ n | n <- [0..], slot n ]
+main :: IO ()
+main = do
+  putStrLn $ "part 1: " ++ show (solution part1)
+  putStrLn $ "part 2: " ++ show (solution part2) 
+  putStrLn "all done"
+
+
+----------------------------------------------------------------------
+-- Problem Input
+
+part1 :: [Disc]
+part1 =
+  zipWith ($)
+    [ createDisc 13 11
+    , createDisc 5 0
+    , createDisc 17 11
+    , createDisc 3 0
+    , createDisc 7 2
+    , createDisc 19 17 ]
+    [ 1.. ]
+
+
+part2 :: [Disc]
+part2 = part1 ++ [ createDisc 11 0 7 ]
+
+
+example :: [Disc]
+example = [ createDisc 5 1 4, createDisc 2 2 1 ]
+
+
+----------------------------------------------------------------------
+-- calculation
+
+solution :: [Disc] -> Time
+solution = head . solutions
+
+
+solutions :: [Disc] -> [Time]
+solutions = foldr1 merge . map zeros
+
 
 zeros :: Disc -> [Time]
-zeros disc@(n, slot) =
-  let fZ = firstZero disc
+zeros disc@(Disc n delta) =
+  let !fZ = firstZero disc
       step = fromIntegral n
-  in [ fZ + n | n <- [0,step..] ]
+  in [ fZ + i | i <- [0,step..] ]
+
+
+firstZero :: Disc -> Integer
+firstZero (Disc n delta) =
+  fromIntegral $ (negate delta) `mod` n
+
+
+createDisc :: Int -> Int -> Int -> Disc
+createDisc posCnt startPos nr =
+  let delta = (nr + startPos)  `mod` posCnt
+  in Disc posCnt delta
 
 
 merge :: Ord a => [a] -> [a] -> [a]
@@ -28,35 +77,3 @@ merge as'@(a:as) bs'@(b:bs)
   | b < a = merge as' bs
   | a == b = a : merge as bs
 merge _ _ = []
-
-
-solutions :: [Disc] -> [Time]
-solutions = foldr1 merge . map zeros
-
-
-solution :: [Disc] -> Time
-solution = head . solutions
-
-
-test :: [Disc]
-test = [ createDisc 5 1 4, createDisc 2 2 1 ]
-
-part1 :: [Disc]
-part1 =
-  [ createDisc 13 1 11
-  , createDisc 5 2 0
-  , createDisc 17 3 11
-  , createDisc 3 4 0
-  , createDisc 7 5 2
-  , createDisc 19 6 17 ]
-
-
-part2 :: [Disc]
-part2 = part1 ++ [ createDisc 11 7 0 ]
-
-
-main :: IO ()
-main = do
-  putStrLn $ "part 1: " ++ show (solution part1)
-  putStrLn $ "part 2: " ++ show (solution part2) 
-  putStrLn "all done"
