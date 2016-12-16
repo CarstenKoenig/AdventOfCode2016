40b14023cda30b3ac2e55428d55c5c6b730315f4
Carsten König <Carsten@gettingsharper.de>
2016-12-16 09:19:27 +0100

    day16 moved to strict ByteStrings - Part2 still to slow in Haskell - F# FTW

--- Day16/Main.hs
+++ Day16/Main.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, OverloadedStrings #-}
 
 module Main where
 
-import Data.List (foldl', unfoldr)
+import Data.List (unfoldr)
+import Data.ByteString.Char8 (ByteString, foldl')
+import qualified Data.ByteString.Char8 as BS
 
-type Data = String
+type Data = ByteString
 
 
 checkSumFor :: Data -> Int -> Data
@@ -15,21 +17,23 @@ checkSumFor from len =
 checkSum :: Data -> Data
 checkSum dt =
   let dt' = reduce dt
-  in if even (length dt')
+  in if even (BS.length dt')
      then checkSum dt'
      else dt'
 
 
 reduce :: Data -> Data
-reduce (x:y:xs)
-  | x == y = '1' : reduce xs
-  | otherwise = '0' : reduce xs
-reduce xs = []
+reduce input =
+  let (bs,rest) = BS.splitAt 2 input
+  in case BS.unpack bs of
+    [x,y] | x == y -> '1' `BS.cons` reduce rest
+          | otherwise -> '0' `BS.cons` reduce rest
+    _ -> BS.empty
 
 
 random :: Data -> Int -> Data
 random from len =
-  take len $ fillUp from len
+  BS.take len $ fillUp from len
 
 
 fillUp :: Data -> Int -> Data
@@ -38,17 +42,17 @@ fillUp from len =
 
 
 dragons :: Data -> [(Int, Data)]
-dragons from = unfoldr uf (length from, from)
+dragons from = unfoldr uf (BS.length from, from)
   where uf x = Just (x, dragon x)
 
 
 dragon :: (Int, Data) -> (Int, Data)
-dragon (len,xs) = (2*len+1, xs ++ ('0' : revInvert xs))
+dragon (len, xs) = (2*len+1, xs `BS.append` ('0' `BS.cons` revInvert xs))
 
 
 revInvert :: Data -> Data
-revInvert = foldl' inv []
-  where inv xs x = inv' x : xs
+revInvert = foldl' inv BS.empty
+  where inv xs x = inv' x `BS.cons` xs
         inv' '1' = '0'
         inv' _ = '1'
 
@@ -66,7 +70,12 @@ targetLength :: Int
 targetLength = 272
 
 
+targetLength2 :: Int
+targetLength2 = 35651584
+
+
 main :: IO ()
 main = do
   putStrLn $ "Part 1: " ++ show (checkSumFor input targetLength)
+  putStrLn $ "Part 2: " ++ show (checkSumFor input targetLength2)
   putStrLn "all done"
