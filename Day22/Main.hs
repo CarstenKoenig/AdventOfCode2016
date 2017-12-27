24d95565bc62f463ba9726393f64663e659c9bf6
Carsten König <carsten@gettingsharper.de>
2017-12-27 21:37:39 +0100

    day 22: read input

--- Day22/Main.hs
+++ Day22/Main.hs
@@ -1,5 +1,40 @@
 module Main where
 
+import Data.Maybe (fromJust)
+import Parser
+
+data Node =
+  Node
+  { nodeCoord :: Coord
+  , nodeSize  :: Size
+  , nodeUsed  :: Size
+  , nodeAvail :: Size
+  } deriving Show
+
+type Coord = (Int, Int)
+type Size = Int
+
 main :: IO ()
 main = do
   putStrLn "all done"
+
+-- need to parse something like this:
+-- Filesystem              Size  Used  Avail  Use%
+-- /dev/grid/node-x0-y0     93T   68T    25T   73%
+
+readNodes :: IO [Node]
+readNodes = map (fromJust . eval nodeP) . drop 2 . lines <$> readFile "input.txt"
+
+
+nodeP :: Parser Node
+nodeP = Node <$> nodeCoordP <*> sizeP <*> sizeP <*> sizeP
+
+
+nodeCoordP :: Parser Coord
+nodeCoordP = (,)
+             <$> (parseString "/dev/grid/node-x" *> parseInt)
+             <*> (parseString "-y" *> parseInt <* ignoreWhiteSpace)
+
+
+sizeP :: Parser Size
+sizeP = parseInt <* parseString "T" <* ignoreWhiteSpace
