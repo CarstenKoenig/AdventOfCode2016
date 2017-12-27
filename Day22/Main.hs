8c39ebf532055fe1cddef0e75b3d9d8816d6b7ba
Carsten König <carsten@gettingsharper.de>
2017-12-27 22:15:36 +0100

    Day 22 - 1 star

--- Day22/Main.hs
+++ Day22/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List (sortBy)
 import Data.Maybe (fromJust)
 import Parser
 
@@ -16,7 +17,24 @@ type Size = Int
 
 main :: IO ()
 main = do
-  putStrLn "all done"
+  inp <- readNodes
+  putStrLn $ "part 1: " ++ show (part1 inp)
+
+
+part1 :: [Node] -> Int
+part1 nodes = go 0 used avail - selfFit
+  where
+    go _ [] _ = 0
+    go n (u:us') as@[a]
+      | u <= a    = (n+1) + go n us' as
+      | otherwise = n + go n us' as
+    go n us@(u:us') as@(a:as')
+      | u > a     = n + go n us' as
+      | otherwise = go (n+1) us as'
+    used = sortBy (flip compare) . filter (> 0) $ map nodeUsed nodes
+    avail = sortBy (flip compare) $ map nodeAvail nodes
+    selfFit = length $ filter (\n -> nodeAvail n >= nodeUsed n && nodeUsed n > 0) nodes
+
 
 -- need to parse something like this:
 -- Filesystem              Size  Used  Avail  Use%
